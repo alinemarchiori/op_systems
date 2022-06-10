e6e4ad3ae03a33e7fb3521433a315412714b0492
Aline Marchiori <alineeuu@outlook.com>
2022-06-09 21:30:02 -0300

    algoritmo por prioridade pronto

--- op_systems/por_prioridade.py
+++ op_systems/por_prioridade.py
@@ -1,82 +1,62 @@
+import time
+from classes import*
 
+lista_processos = []
+lista_ordenada  = []
+lista_de_processos_para_mostrar_o_tempo = None
 
-#Transforma o arquivo em uma lista
-def ler_arq():
-  arq = open('prioridades.txt', 'r')
-  lista = arq.readlines()
-  new_lista =[]
-  for linha in lista:
-    linha = linha.rstrip('\n')
-    new_lista.append(linha)
-  arq = arq.close()
-  return new_lista
+def getNumeroProcessosFaltantes():
+    print(f'Processos em espera: {len(lista_ordenada)}\n')
 
-#Encontra o tipo da lista
-def type_alg():
-  list_of_process = ler_arq()
-  type_my_alg = list_of_process[0]
-  selection = type_my_alg.split("|")
-  return selection
+def ordena_a_lista():
+  global lista_ordenada
+  lista_ordenada = sorted(lista_processos, key = lambda x:(x.prioridade, x.tempo_execucao))
 
-#lista de listas
-def list_of_list():
-  arq = ler_arq()
-  arq.pop(0)
-  new_list =[]
-  tam = len(arq)
-  for i in range(tam):
-    new_list.append([arq[i]])
-  arq = []
-  for i in range(tam):
-    aux = new_list[i]
-    aux_1 = aux[0]
-    arq.append(aux_1.split("|"))
-  for i in arq:
-    i[1],i[2],i[3],i[4],i[5] = int(i[1]),int(i[2]),int(i[3]),int(i[4]),int(i[5])
-  return arq
+def criaProcesso():
+    processo = input()
+    global lista_processos
+    objeto_processo = Processo()
+    objeto_processo.setDetalhesProcesso(processo.split('|'))
+    lista_processos.append(objeto_processo)
+    lista_de_processos_para_mostrar_o_tempo.append(objeto_processo)
+    ordena_a_lista()
 
-#organizando por prioridade e por menor tempo
-def org_priority():
-  my_list = list_of_list()
-  my_list_sorted = sorted(my_list, key = lambda x:(x[3], x[2]))
-  return my_list_sorted
+def listaProcessos(lista_linhas):
+  global lista_processos
+  for processo in lista_linhas:
+    objeto_processo = Processo()
+    objeto_processo.setDetalhesProcesso(processo.split('|'))
+    lista_processos.append(objeto_processo)
 
-def temp(period):
-    list_sorted = select_priority()
-    print(list_sorted)
-    #for process in list_sorted:
-     #   process[2] -= int(period)
-      #  repre(process)
-       # if process[2] <= 0:
-        #    break
+def define_prioridade():
+  global lista_ordenada, lista_processos
+  minha_prioridade = lista_ordenada[0].prioridade
+  elementos_prioritarios = []
+  for objeto_do_processo in lista_ordenada:
+      if objeto_do_processo.prioridade == minha_prioridade:
+        elementos_prioritarios.append(objeto_do_processo)
+      else:
+        break
+  for objeto_do_processo in elementos_prioritarios:
+    lista_processos.remove(objeto_do_processo)
+    lista_ordenada.remove(objeto_do_processo)
+  return elementos_prioritarios
 
-def select_priority():
-    list_sorted = org_priority()
-    first_priority = list_sorted[0][3]
-    list_element_priority = []
-    for element in list_sorted:
-        if element[3] == first_priority:
-            list_element_priority.append(element)
-            print(list_element_priority)
-            global_list.remove(element)   
-        else:
-            break
-    return list_element_priority
-
-def repre(process):
-  element_list = process
-  print('Process Number: ' + element_list[0] + ' ID Process: ' +  str(element_list[1]) + 'Period:' + str(element_list[2]))
-
-#função principal
-def main():
-  alg_esc = type_alg()
-  if (alg_esc[0] == 'prioridade'):
-    period = alg_esc[1]
-    while len(global_list) >= 0:
-        temp(period)
-    
-
-  
-global_list_aux = org_priority()
-global_list = global_list_aux
-main()
+def porPrioridade(lista_de_linhas):
+  global lista_de_processos_para_mostrar_o_tempo
+  prioridade, tempo_de_CPU = lista_de_linhas[0].split("|")
+  lista_de_linhas.remove(lista_de_linhas[0])
+  listaProcessos(lista_de_linhas)
+  lista_de_processos_para_mostrar_o_tempo = lista_processos
+  ordena_a_lista()
+  while len(lista_processos) > 0:
+    lista_elementos_prioritarios = define_prioridade()
+    while len(lista_elementos_prioritarios) > 0:
+      for processo in lista_elementos_prioritarios:
+          processo.atualizaTempo(int(tempo_de_CPU))
+          print(processo)
+          time.sleep(1)
+          if processo.getTempo() <= 0:
+            lista_elementos_prioritarios.remove(processo)
+          getNumeroProcessosFaltantes()
+  return lista_de_processos_para_mostrar_o_tempo
