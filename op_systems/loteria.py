2a7c4cca1f806b8efb811142f43ccb0be7d60b3d
Aline Marchiori <49764122+alinemarchiori@users.noreply.github.com>
2022-06-25 19:00:02 -0300

    versão final

--- op_systems/loteria.py
+++ op_systems/loteria.py
@@ -1,6 +1,3 @@
-############################################################# NÃO ESTÁ PRONTO ############################################################
-#falta testar função para o input em tempo de execução
-
 import random
 import time
 
@@ -16,27 +13,30 @@ tempo_decorrido = 0
 Nome= []
 Tempo= []
 
-def criaProcesso():
-    processo = input()
-    global nome_do_processo, tempo_de_cada_processo, lista_de_bilhetes_processo
-    lista_elementos_do_processo = processo.split("|")
-    nome_do_processo.append(lista_elementos_do_processo[0])
-    tempo_de_cada_processo.append(int(lista_elementos_do_processo[2]))
-    lista_de_bilhetes_processo.append(int(lista_elementos_do_processo[3]))
+def criaProcesso_loteria():
+    while True:
+        processo = input()
+        global nome_do_processo, tempo_de_cada_processo, lista_de_bilhetes_processo
+        lista_elementos_do_processo = processo.split("|")
+        nome_do_processo.append(lista_elementos_do_processo[0])
+        tempo_de_cada_processo.append(int(lista_elementos_do_processo[2]))
+        lista_de_bilhetes_processo.append(int(lista_elementos_do_processo[3]))
 
 def loteria(lista_linhas):
-    global nome_do_processo, tempo_de_cada_processo, lista_de_bilhetes_processo,cpu
-    global total_de_bilhetes, lista_bilhetes_teto,lista_de_bilhetes,tempo_decorrido,Nome,Tempo
+    global nome_do_processo, tempo_de_cada_processo, lista_de_bilhetes_processo, cpu
+    global total_de_bilhetes, lista_bilhetes_teto, lista_de_bilhetes, tempo_decorrido, Nome, Tempo
     
     loteria, cpu = lista_linhas[0].split("|")
     lista_linhas.remove(lista_linhas[0])
     cpu = int(cpu)
-    #pegando os dados inceridos
+
+    #pegando os dados inseridos
     for linha in lista_linhas:
         lista_elementos_do_processo = linha.split("|")
         nome_do_processo.append(lista_elementos_do_processo[0])
         tempo_de_cada_processo.append(int(lista_elementos_do_processo[2]))
         lista_de_bilhetes_processo.append(int(lista_elementos_do_processo[3]))
+        
 
     #total de bilhetes 
     for i in lista_de_bilhetes_processo:
@@ -97,10 +97,11 @@ def loteria(lista_linhas):
         if tempo_de_cada_processo[contador_de_intervalo] <=0:
             Nome.append(nome_do_processo[contador_de_intervalo])
             Tempo.append(tempo_decorrido)
+            del(nome_do_processo[contador_de_intervalo])
             del(tempo_de_cada_processo[contador_de_intervalo])
             del(lista_de_bilhetes_processo[contador_de_intervalo])
-            del(lista_bilhetes_teto[contador_de_intervalo])
-            #substiturir pela função total de bilhetes
+
+           #substiturir pela função total de bilhetes
             total_de_bilhetes = 0
             #total de bilhetes 
             for i in lista_de_bilhetes_processo:
@@ -115,3 +116,16 @@ def loteria(lista_linhas):
             while i<=total_de_bilhetes:
                 lista_de_bilhetes.append(i)
                 i+=1
+            print(lista_de_bilhetes)
+
+            lista_bilhetes_teto=[]
+            p = 0 #posição na lista de processos
+            contador_de_bilhetes=None
+            while total_de_bilhetes>0:
+                if contador_de_bilhetes ==None:
+                    contador_de_bilhetes = lista_de_bilhetes_processo[p]
+                else:
+                    contador_de_bilhetes += lista_de_bilhetes_processo[p]
+                lista_bilhetes_teto.append(contador_de_bilhetes)
+                total_de_bilhetes-=lista_de_bilhetes_processo[p]
+                p+=1
