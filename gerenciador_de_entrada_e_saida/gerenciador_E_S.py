64b5a658cbf5757f2f2ffee0dbd3ebddafa74c66
Aline Marchiori <aline.marchiori@crearesistemas.com.br>
2022-10-17 10:10:34 -0300

    modificacoes feitas na aula

--- gerenciador_de_entrada_e_saida/gerenciador_E_S.py
+++ gerenciador_de_entrada_e_saida/gerenciador_E_S.py
@@ -1,5 +1,42 @@
-
+from escalonador import *
+from threading import Thread, Semaphore, Lock
+import random
 # tabela de processos -> PRONTOS E BLOQUEADOS
+lista_processos_prontos = []
+lista_processos_bloqueados = []
+lista_de_dispositivos = []
+
+class Dispositivo:
+    def __init__(self, id, num_usos_simultaneos, tempo_operacao):
+        self.id = id
+        self.num_usos_simultaneos = num_usos_simultaneos
+        self.tempo_operacao = tempo_operacao
+        self.thread = Thread(target=self._on_run)
+        self.semmaphore = Semaphore(self.num_usos_simultaneos)
+
+    def _on_run(self):
+        with self.semmaphore:
+            pass
+
+def main():
+    linhas_do_arquivo = leArquivo("nomedoarquivo.txt")
+    tempo_de_cpu, num_de_dispositivos = linhas_do_arquivo[0].split("|")
+    
+    for linha_do_arquivo in range(1,(int(num_de_dispositivos)+1)):
+        linha_separada = linha_do_arquivo.split("|")
+        id = linha_separada[0]
+        num_usos_simultaneos = linha_separada[1]
+        tempo_operacao = linha_separada[2]
+        lista_de_dispositivos.append(Dispositivo(id, num_usos_simultaneos, tempo_operacao))
+    
+    processo_em_execucao = alternanciaCircular()
+    chance_de_requisitar = 80#TODO: pegar esse valor do arquivo
+    chance_de_requisitar = chance_de_requisitar/100
+    if random.random() < chance_de_requisitar:
+        #TODO: botar o processo como bloqueado, e sortear o dispositivo e momento que ele deve ser executado.
+        pass
+    else:
+        alternanciaCircular()
 
 
 # lê arquivo
@@ -29,3 +66,14 @@ fazer_entrada_e_saida():
         executa E/S
         processo => PRONTO
 """
+
+
+
+# tarefas:
+
+# ler e tratar os dados do arquivo
+# enumerar os dispositivos (uma thread para cada dispositivo)
+# bloquear os dispositivos que estão no seu limite de processos
+# controlar o tempo de cada processo (executando e fazendo E/S)
+# atualizar lista de bloqueado e pronto
+# 
